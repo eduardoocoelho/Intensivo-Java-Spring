a5ea0bb37522ca0a970afb33329116f9eeb75e9f
Eduardo Coelho <eceduardocoelho@gmail.com>
2023-05-10 15:56:00 -0300

    Id search created

--- src/main/java/com/intensivo/dslist/controllers/GameController.java
+++ src/main/java/com/intensivo/dslist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.intensivo.dslist.dto.GameDTO;
 import com.intensivo.dslist.dto.GameMinDTO;
 import com.intensivo.dslist.services.GameService;
 
@@ -20,6 +22,12 @@ public class GameController {
 	@GetMapping //Tipo da requisição (GET)
 	public List<GameMinDTO> getAll(){
 		List<GameMinDTO> result = gameService.getAll();
+		return result; 
+	}
+	
+	@GetMapping(value = "/{id}") //Irá receber um id na rota
+	public GameDTO getById(@PathVariable Long id){ //Indica que o parametro seja correspondente com id da requisição
+		GameDTO result = gameService.getById(id);
 		return result; 
 	}
 }
--- src/main/java/com/intensivo/dslist/services/GameService.java
+++ src/main/java/com/intensivo/dslist/services/GameService.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.intensivo.dslist.dto.GameDTO;
 import com.intensivo.dslist.dto.GameMinDTO;
+import com.intensivo.dslist.entities.Game;
 import com.intensivo.dslist.repositories.GameRepository;
 
 @Service //Registrar como componente do sistema
@@ -14,8 +17,16 @@ public class GameService {
 	@Autowired //Injetar uma instância de GameRepository para chamada
 	private GameRepository gameRepository; 
 	
+	@Transactional(readOnly = true) //Boa prática - garantir que a operação do banco vai acontecer
+	//obedecendo aos princípios de transação (readOnly = true -> apenas leitura)
 	public List<GameMinDTO> getAll(){
 		List <GameMinDTO> result = gameRepository.findAll().stream().map(x -> new GameMinDTO(x)).toList(); //Transformar todo Game x para GameMinDTO
 		return result;
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO getById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
 	}
 }
